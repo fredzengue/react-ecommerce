00ce9b63cf409a1a13d0519fcacb72cdb799951d
unknown <moubarakhosny636@gmail.com>
2022-03-24 15:33:09 +0100

    create Layout, Oders, Profile components

---
+++ src/components/Dashboard/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Oders from './Oders';
+import Profile from './Profile';
+
+const Layout = () => {
+    return (
+        <div>
+            <h1>Layout</h1>
+            <Oders/>
+            <Profile/>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/components/Dashboard/Oders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Oders = () => {
+    return (
+        <div>
+            <h3 className='sous-titre'>
+                ODERS</h3>
+        </div>
+    );
+};
+
+export default Oders;
