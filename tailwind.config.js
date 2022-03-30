f037d2b2ebfd8f4e2d242c77a4a67a9f1ae5dc19
fred zengue <fredzengue@gmail.com>
2022-03-30 14:47:38 +0200

    styling navbar and footer color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -5,7 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      fontFamily: {
+        'sans': ['-apple-system', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        'close_btn' : '0 2px 6px rgb(0 0 0 / 50%)'
+      }
     },
     colors: {
       'white': '#fff',
